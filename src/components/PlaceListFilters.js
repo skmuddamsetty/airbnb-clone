4d6fc382161da394e144916a9b2666ca64c7f4f1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-23 07:37:24 -0500

    partially completed date range picker

--- src/components/PlaceListFilters.js
+++ src/components/PlaceListFilters.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { setTextFilter, sortByDate, sortByPrice } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
 
-const PlaceListFilters = props => (
-  <div>
-    <input
-      type='text'
-      value={props.filters.text}
-      onChange={e => {
-        props.dispatch(setTextFilter(e.target.value));
-      }}
-    />
-    <select
-      value={props.filters.sortBy}
-      onChange={e => {
-        if (e.target.value === 'date') {
-          props.dispatch(sortByDate());
-        } else {
-          props.dispatch(sortByPrice());
-        }
-      }}
-    >
-      <option value='date'>Date</option>
-      <option value='price'>Price</option>
-    </select>
-  </div>
-);
+class PlaceListFilters extends React.Component {
+  state = {
+    calendarFocused: null
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type='text'
+          value={this.props.filters.text}
+          onChange={e => {
+            this.props.dispatch(setTextFilter(e.target.value));
+          }}
+        />
+        <select
+          value={this.props.filters.sortBy}
+          onChange={e => {
+            if (e.target.value === 'date') {
+              this.props.dispatch(sortByDate());
+            } else {
+              this.props.dispatch(sortByPrice());
+            }
+          }}
+        >
+          <option value='date'>Date</option>
+          <option value='price'>Price</option>
+        </select>
+        <DateRangePicker
+          startDate={this.props.filters.startDate}
+          endDate={this.props.filters.endDate}
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
