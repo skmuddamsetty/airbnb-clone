93abba145fcd1e0a2c372ef7998c46b6eb407cc8
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-25 16:47:42 -0500

    booking options box

--- src/components/BookingOptionsBox/BookingOptionsBox.js
+++ src/components/BookingOptionsBox/BookingOptionsBox.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import PriceFormatter from '../Utils/PriceFormatter';
 import Divider from '../Divider';
+import { DateRangePicker } from 'react-dates';
+import { setStartDate, setEndDate } from '../../actions/filters';
+import { connect } from 'react-redux';
+
 class BookingOptionsBox extends React.Component {
+  state = {
+    calendarFocused: null
+  };
+  datesChangeHandler = ({ startDate, endDate }) => {
+    this.props.dispatch(setStartDate(startDate));
+    this.props.dispatch(setEndDate(endDate));
+  };
+  calendarFocusChangeHandler = calendarFocused => {
+    this.setState(() => ({ calendarFocused }));
+  };
   render() {
     return (
       <div className='booking-options-box'>
         <PriceFormatter price={this.props.place.price} />
         <Divider />
+        <div className='booking-options-box__date-range-picker'>
+          <DateRangePicker
+            startDate={this.props.filters.startDate}
+            endDate={this.props.filters.endDate}
+            onDatesChange={this.datesChangeHandler}
+            focusedInput={this.state.calendarFocused}
+            onFocusChange={this.calendarFocusChangeHandler}
+            numberOfMonths={1}
+            showClearDates={true}
+          />
+        </div>
         <button className='button' type='submit'>
           Reserve
         </button>
@@ -14,4 +39,9 @@ class BookingOptionsBox extends React.Component {
     );
   }
 }
-export default BookingOptionsBox;
+const mapStateToProps = state => {
+  return {
+    filters: state.filters
+  };
+};
+export default connect(mapStateToProps)(BookingOptionsBox);
