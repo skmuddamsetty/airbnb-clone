6afa8f5f663ccd556e5a80ca563f3b60fa1a2851
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-25 11:13:44 -0500

    added place plan overview to the list

--- src/components/PlaceListItem.js
+++ src/components/PlaceListItem.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
+import PlacePlanOverview from './PlaceDetails/PlacePlanOverview';
 
 const PlaceListItem = props => (
   <Link to={`/place-view/${props.place.id}`} className='list-item'>
     <div>
       <h3 className='list-item__title'>{props.place.title}</h3>
+      <div className='place-plan-overview__list-item'>
+        <PlacePlanOverview place={props.place} />
+      </div>
       <span className='list-item__sub-title'>
         {moment(props.place.createdAt).format('MMMM Do, YYYY')}
       </span>
