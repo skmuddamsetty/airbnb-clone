62582a90f2dde462428da7883790f081451666b6
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-25 09:27:38 -0500

    added styles to the page header

--- src/components/AddHostingPlace.js
+++ src/components/AddHostingPlace.js
@@ -2,10 +2,20 @@ import React from 'react';
 import PlaceForm from './PlaceForm';
 import { addPlace } from '../actions/places';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const AddHostingPlacePage = props => (
   <div>
-    <h1>Add Place Page</h1>
+    <div className='page-header'>
+      <div className='content-container'>
+        <h1 className='page-header__title'>Add Place Page</h1>
+        <div className='page-header__actions'>
+          <Link className='button' to='/'>
+            Home
+          </Link>
+        </div>
+      </div>
+    </div>
     <div className='content-container'>
       <PlaceForm
         onSubmit={place => {
