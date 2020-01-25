ea741b47dd64906e28c3185a33c943b20388e3d3
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-25 10:54:34 -0500

    added numeral

--- src/components/PlaceListItem.js
+++ src/components/PlaceListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const PlaceListItem = props => (
   <Link to={`/place-view/${props.place.id}`} className='list-item'>
     <div>
       <h3 className='list-item__title'>{props.place.title}</h3>
-      <span className='list-item__sub-title'>{props.place.createdAt}</span>
+      <span className='list-item__sub-title'>
+        {moment(props.place.createdAt).format('MMMM Do, YYYY')}
+      </span>
     </div>
-    <h3 className='list-item__data'>{props.place.price}</h3>
+    <h3 className='list-item__data'>
+      {numeral(props.place.price / 100).format('$0,0.00')}
+    </h3>
   </Link>
 );
 
