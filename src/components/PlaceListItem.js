169b1b4756e36b8b16db8eb54998f775b54099f1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-25 10:29:17 -0500

    added styles to the list items

--- src/components/PlaceListItem.js
+++ src/components/PlaceListItem.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PlaceListItem = props => (
-  <div>
-    <Link to={`/place-view/${props.place.id}`}>
-      <strong>Place Name:</strong>
-      {props.place.title}
-    </Link>
+  <Link to={`/place-view/${props.place.id}`} className='list-item'>
     <div>
-      <strong>Price:</strong>
-      {props.place.price}
+      <h3 className='list-item__title'>{props.place.title}</h3>
+      <span className='list-item__sub-title'>{props.place.createdAt}</span>
     </div>
-    <div>
-      <strong>Created At:{props.place.createdAt}</strong>
-    </div>
-  </div>
+    <h3 className='list-item__data'>{props.place.price}</h3>
+  </Link>
 );
 
 export default PlaceListItem;
--- src/components/PlacesList.js
+++ src/components/PlacesList.js
@@ -4,11 +4,23 @@ import PlaceListItem from './PlaceListItem';
 import getVisiblePlaces from '../selectors/places';
 
 const PlacesList = props => (
-  <div>
-    <h1>Places List</h1>
-    {props.places.map(place => {
-      return <PlaceListItem place={place} key={place.id} />;
-    })}
+  <div className='content-container'>
+    <div className='list-header'>
+      <div className='show-for-mobile'>Hosting Places</div>
+      <div className='show-for-desktop'>Hosting Place</div>
+      <div className='show-for-desktop'>Price per night</div>
+    </div>
+    <div className='list-body'>
+      {props.places && props.places.length === 0 ? (
+        <div className='list-item list-item__message'>
+          <span>No Hosting Places</span>
+        </div>
+      ) : (
+        props.places.map(place => {
+          return <PlaceListItem place={place} key={place.id} />;
+        })
+      )}
+    </div>
   </div>
 );
 
