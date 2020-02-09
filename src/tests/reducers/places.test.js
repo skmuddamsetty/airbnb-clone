1ed99db14a3f44c1316538d12a7c6a90ff74b0f6
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-02-08 20:17:05 -0500

    completed reducers testing

--- src/tests/reducers/places.test.js
+++ src/tests/reducers/places.test.js
@@ -14,3 +14,45 @@ test('should remove place by id', () => {
   const state = placesReducer(places, action);
   expect(state).toEqual([places[0], places[2]]);
 });
+
+test('should not remove place if id not found', () => {
+  const action = {
+    type: 'REMOVE_PLACE',
+    id: -1
+  };
+  const state = placesReducer(places, action);
+  expect(state).toEqual(places);
+});
+
+test('should add a place', () => {
+  const action = {
+    type: 'ADD_PLACE',
+    place: places[1]
+  };
+  const state = placesReducer(places, action);
+  expect(state).toEqual([...places, action.place]);
+});
+
+test('should edit a place with id', () => {
+  const action = {
+    type: 'EDIT_PLACE',
+    updates: {
+      title: 'John Doe Apartments'
+    },
+    id: places[0].id
+  };
+  const state = placesReducer(places, action);
+  expect(state[0].title).toEqual(action.updates.title);
+});
+
+test('should not edit place if id not found', () => {
+  const action = {
+    type: 'EDIT_PLACE',
+    updates: {
+      title: 'John Doe Apartments'
+    },
+    id: -1
+  };
+  const state = placesReducer(places, action);
+  expect(state).toEqual(places);
+});
